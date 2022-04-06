78acbbd285e2638a3170009768f9ef7060ec4a86
FENGMIAO <13631030360@qq.com>
2022-04-07 00:22:37 +0800

    Rectangle_xiugai1

--- RectangleApplication/Program.cs
+++ RectangleApplication/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RectangleApplication
+{
+    class Rectangle
+    {
+        //成员变量
+        double length;
+
+        double width;
+
+        //构造函数
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("该矩形的长为: {0}", length);
+            Console.WriteLine("该矩形的宽为: {0}", width);
+            Console.WriteLine("该矩形的面积为: {0}", GetArea());
+        }
+    } //矩形类结束
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入矩形的长和宽:");
+            double len = Convert.ToDouble(Console.ReadLine());
+            double wid = Convert.ToDouble(Console.ReadLine());
+            Rectangle r = new Rectangle(len, wid);
+            r.Display();
+            Console.ReadKey();
+        }
+    }
+}
